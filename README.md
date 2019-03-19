d8e51708ea3de0718f4fe4057f62238753d4ce2f
Jaiswal, Sumit <sumijaiswal@paypal.com>
2019-03-19 16:41:37 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# UseProcessor
+A simple example of a Processing sketch running as an Intellij project.
+
+For more: http://processing.org/tutorials/eclipse/
---
+++ src/main/java/processing/core/UsingProcessing.java
@@ -0,0 +1,23 @@
+package processing.core;
+
+import processing.core.PApplet;
+
+public class UsingProcessing extends PApplet {
+
+    public static void main(String args[]){
+        PApplet.main("processing.core.UsingProcessing");
+    }
+
+    public void settings(){
+        size(300,300);
+    }
+
+    public void setup(){
+        fill(120,50,240);
+    }
+
+    public void draw(){
+        ellipse(width/2,height/2,second(),second());
+    }
+}
+
